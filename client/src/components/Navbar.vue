46abbd380bd4b9836cfcbb3e12db818bda4599a2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-11-29 19:22:10 +0530

    :zap: Designing Login and Register Page

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -8,7 +8,7 @@
           <ul class="flex items-center">
               <li class="p-2 mx-4 dark:text-white"><router-link to="/">Home</router-link></li>
               <li class="p-2 mx-4 dark:text-white"><router-link to="/about">About</router-link></li>
-              <li class="p-2 mx-4"><button class="rounded-full bg-purple-600 border-purple-600 px-6 py-2 text-white">Register</button></li>
+              <li class="p-2 mx-4"><a href="/register" class="rounded-full bg-purple-600 border-purple-600 px-6 py-2 text-white">Register</a></li>
           </ul>
       </div>
     </div>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -2,8 +2,15 @@
   <div class="register bg-gray-200 dark:bg-black">
     <div class="flex justify-center items-center h-full">
       <div class="flex max-w-4xl">
-        <div class="bg-white w-1/2 p-8">
-          <h1 class="text-gray-700 text-3xl text-center">Login</h1>
+        <div class="bg-white w-full lg:w-1/2 p-8">
+          <h1 class="text-gray-700 text-3xl text-center">Sign Up</h1>
+          <div class="my-3">
+            <input
+              type="text"
+              placeholder="Username"
+              class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline"
+            />
+          </div>
           <div class="my-3">
             <input
               type="email"
@@ -18,7 +25,7 @@
               class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline"
             />
           </div>
-          <div class="my-3 flex justify-between">
+          <div class="my-3 flex justify-between sm:flex-col lg:flex-row">
             <label for=""><input type="checkbox" />Remember Me</label>
             <a href="#" class="text-blue-400">Forgot Email or Password?</a>
           </div>
@@ -26,11 +33,11 @@
             <button
               class="rounded-full bg-blue-600 border-blue-600 px-8 py-2 text-white w-1/2"
             >
-              Login
+              Register
             </button>
           </div>
         </div>
-        <div class="w-1/2">
+        <div class="w-1/2 hidden lg:block">
           <img
             src="https://images.unsplash.com/photo-1534531688091-a458257992cb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bWVtb3JpZXN8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"
             class="w-full h-full"
