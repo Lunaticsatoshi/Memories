9f135f18b19c294e54cc2d988216fd55784348e2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-12-06 16:56:31 +0530

    :zap: Login Page connection

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,23 +3,44 @@
     <div class="flex justify-center items-center h-full">
       <div class="flex max-w-4xl mt-20">
         <div class="bg-white p-8 w-full lg:w-1/2">
-            <h1 class="text-gray-700 text-3xl text-center">Login</h1>
+          <h1 class="text-gray-700 text-3xl text-center">Login</h1>
+          <form @submit.prevent = "loginUser">
             <div class="my-3">
-                <input type="email" placeholder="Email" v-model="email" class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline" />
+              <input
+                type="email"
+                placeholder="Email"
+                v-model="email"
+                class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline"
+              />
             </div>
             <div class="my-3">
-                <input type="password" placeholder="Password" v-model="password" class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline" />
+              <input
+                type="password"
+                placeholder="Password"
+                v-model="password"
+                class="border-rounded w-full py-2 px-4 outline-none focus:shadow-outline"
+              />
             </div>
             <div class="my-3 flex justify-between sm:flex-col lg:flex-row">
-                <label for=""><input type="checkbox" />Remember Me</label>
-                <a href="#" class="text-blue-400">Forgot Email or Password?</a>
+              <label for=""><input type="checkbox" />Remember Me</label>
+              <a href="#" class="text-blue-400">Forgot Email or Password?</a>
             </div>
             <div class="my-3 flex justify-center items-center">
-                <button class="rounded-full bg-blue-600 border-blue-600 px-8 py-2 text-white w-1/2">Login</button>
+              <input
+                type="submit"
+                value="Login"
+                @submit="loginUser"
+                class="rounded-full bg-blue-600 border-blue-600 px-8 py-2 text-white w-1/2"
+              />
             </div>
+          </form>
         </div>
         <div class="w-1/2 hidden lg:block">
-            <img src="https://images.unsplash.com/photo-1534531688091-a458257992cb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bWVtb3JpZXN8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60" class="w-full h-full" alt="Login">
+          <img
+            src="https://images.unsplash.com/photo-1534531688091-a458257992cb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bWVtb3JpZXN8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"
+            class="w-full h-full"
+            alt="Login"
+          />
         </div>
       </div>
     </div>
@@ -27,13 +48,31 @@
 </template>
 
 <script>
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 export default {
-  data(){
-    return{
+  data() {
+    return {
       email: "",
-      password: ""
+      password: "",
     };
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async loginUser() {
+      console.log(this.email, this.password, "login User");
+      // try {
+      //   let user = {
+      //     email: this.email,
+      //     password: this.password,
+      //   };
+      //   let res = this.login(user);
+      //   if (res.data.success) {
+      //     this.$router.push("/home");
+      //   }
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    },
   },
 };
 </script>
